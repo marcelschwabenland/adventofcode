d446a65d90150d512cb25ea87c85d618f8b3da2f
Marcel Schwabenland <marcel.schwabenland@icloud.com>
2022-12-04 12:14:41 +0100

    Implement Day 4 Part 1 & 2

---
+++ 2022/04/camp_cleanup_part2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define FILEPATH "C:\\Users\\schwa\\Projekte\\adventofcode\\2022\\04\\input.txt"
+
+int main(int argc, char const *argv[]) {
+  FILE * fptr;
+
+  int section_start_a, section_end_a, section_start_b, section_end_b;
+  int overlapping;
+
+  fptr = fopen(FILEPATH, "r");
+  if(fptr == NULL) {
+    return 1;
+  }
+
+  while (feof(fptr) == 0) {
+    // make sure format is matched
+    if(fscanf(fptr, "%d-%d,%d-%d", &section_start_a, &section_end_a, &section_start_b, &section_end_b) > 0) {
+      if(section_start_a >= section_start_b && section_end_b >= section_start_a) {
+        overlapping++;
+      } else if (section_start_a <= section_start_b && section_end_a >= section_start_b) {
+        overlapping++;
+      }
+    }    
+  }
+  
+  printf("%d sections are fully overlapping.", overlapping);
+  return 0;
+}
