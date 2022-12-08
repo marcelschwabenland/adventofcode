b0f3ce0e9e9426da495cca22b4c6ec9f89c83a67
Marcel Schwabenland <marcel.schwabenland@icloud.com>
2022-12-08 19:51:12 +0100

    Solve No Space Left Puzzle

--- 2022/07/no_space_left.c
+++ 2022/07/no_space_left.c
@@ -6,144 +6,121 @@
 #define BUFFERSZ 128
 
 struct node {
-  int value;
+  int size;
+  int num_children;
   char name[BUFFERSZ];
-  struct node *pred;
-  struct node *succ;
-  struct node *parent;
-  struct node *firstchild;
+  struct node *children;
 };
 
-void print_directory_tree(struct node *tree) {
-  int indent = 0;
-  struct node *curr = tree;
-  char out[4096] = {'\0'};
-  
-  while (curr->firstchild != NULL || curr->succ != NULL) {
-    if(curr->firstchild != NULL && curr->succ != NULL) {
-      printf("d %s", curr->name);
-      struct node *child = curr->firstchild;
-      while(child->succ != NULL) {
-        for(int i = 0; i < indent; i++) {
-          printf(" ");
-        }
-        printf("%s", child->name);
-        child = child->succ;
-      }
+void generate_directory(FILE *fptr, fpos_t *fpos, char *buffer, char *cmp, struct node *directory_node) {
+  int sub_directory_count = 0;
+  // search for "$ cd dirname"
+  while (strcmp(buffer, cmp) != 0) {
+    fgets(buffer, BUFFERSZ, fptr);
+  }
+  // skip to first element of directory
+  fgets(buffer, BUFFERSZ, fptr); // read next line which should be "$ ls"
+  fgets(buffer, BUFFERSZ, fptr); // first item in directory
+  // iterate through items of directory and handle files and sub-directories
+  while (buffer[0] != '$') {
+    // handle directories and files
+    if (buffer[0] > '0' && buffer[0] <= '9') {
+      // found a file, add size to directory size
+      directory_node->size += atoi(buffer);
+    } else if (buffer[0] == 'd') {
+      // found a sub-directory, create new directory node
+      // and call generate_directory to work on sub-directory
+      fpos_t *new_fpos = malloc(sizeof(fpos_t));
+      char command[BUFFERSZ] = {'$', ' ', 'c', 'd', ' ', '\0'};
+      char *new_cmp = strcat(command, buffer + 4);
+      struct node *new_directory = malloc(sizeof(struct node));
+      // prepare new directory node
+      strcpy(new_directory->name, buffer + 4);
+      new_directory->size = 0;
+      new_directory->num_children = 0;
+      new_directory->children = malloc(sizeof(struct node));
+      // remember position in file to reset to
+      fgetpos(fptr, new_fpos);
+      // work on sub-directory
+      generate_directory(fptr, new_fpos, buffer, new_cmp, new_directory);
+      // add sub-directory size to current directory
+      directory_node->size += new_directory->size;
+      // add sub-directory to list of children
+      sub_directory_count++;
+      // reallocate memory for new list of children
+      directory_node->children = realloc(directory_node->children, (sizeof(struct node) * sub_directory_count));
+      directory_node->children[sub_directory_count - 1] = *new_directory;
+      // file position pointer was only need in the generate_directory call
+      free(new_fpos);
     }
+    if(feof(fptr) != 0) {
+      break;
+    }
+    fgets(buffer, BUFFERSZ, fptr); // next item in directory
+  }
+  // free memory for list of children if non exist
+  if(sub_directory_count == 0) {
+    free(directory_node->children);
+    directory_node->children = NULL;
+  } else {
+    directory_node->num_children = sub_directory_count;
+  }
+  // reset position in file
+  fsetpos(fptr, fpos);
+  return;
+}
+
+void calculate_total_size_of_directories(struct node *root, int *total_size) {
+  struct node *curr = root;
+  if (curr->size <= 100000) {
+    *total_size += curr->size;
+  }
+  for (int i = 0; i < curr->num_children; i++) {
+    calculate_total_size_of_directories(&curr->children[i], total_size);
   }
 }
 
-int build_directory_tree(FILE *fptr, struct node *subtree) {
-  int subsum = 0;
-  char buffer[BUFFERSZ] = {'\0'};
-  
-  int depth = 0;
-  
-  while (feof(fptr) == 0) {
-    if(buffer[0] != '$' || buffer[5] == '/') {
-      fgets(buffer, BUFFERSZ, fptr);
-    }
-    printf("%s", buffer);
-    if(buffer[0] == '$' && buffer[2] == 'c' && buffer[3] == 'd' && buffer[5] != '.' && buffer[5] != '/') {
-      // increase depth
-      // go to next line
-      subtree = subtree->firstchild;
-      while(subtree->succ != NULL && strcmp(subtree->name, buffer + 5) != 0) {
-        subtree = subtree->succ;
-      }
-      printf("%s\n", subtree->name);
-      fgets(buffer, BUFFERSZ, fptr);
-      // depth++;
-    } else if (buffer[0] == '$' && buffer[2] == 'c' && buffer[3] == 'd' && buffer[5] == '.') {
-      // decrease depth
-      // go to next line
-      subtree = subtree->parent;
-      fgets(buffer, BUFFERSZ, fptr);
-      // depth--;
-    } else if (buffer[0] == '$' && buffer[2] == 'l') {
-      // go to next line
-      fgets(buffer, BUFFERSZ, fptr);
-      printf("%s", buffer);
-      // loop to next command symbol
-      while (buffer[0] != '$') {
-        if(buffer[0] > '0' && buffer[0] <= '9') {
-          // file size add to directory value
-          subtree->value += atoi(buffer);
-        } else if (buffer[0] == 'd' && subtree->firstchild == NULL) {
-          // remember file position
-          // fpos_t *directory_pos = malloc(sizeof(fpos_t));
-          // fgetpos(fptr, directory_pos);
-          // create new directory
-          struct node *firstchild = malloc(sizeof(struct node));
-          strcpy(firstchild->name, buffer + 4);
-          firstchild->value = 0;
-          firstchild->parent = subtree;
-          firstchild->pred = firstchild->succ = firstchild->firstchild = NULL;
-          // append new directory to parent
-          subtree->firstchild = firstchild;
-          // build subtree of firstchild
-          // recursive call here
-          // clean up
-          // fsetpos(fptr, directory_pos);
-          // free(directory_pos);
-        } else if (buffer[0] == 'd' && subtree->firstchild != NULL) {
-          // remember file position
-          // fpos_t *directory_pos = malloc(sizeof(fpos_t));
-          // fgetpos(fptr, directory_pos);
-          // find last child node
-          struct node *pred = subtree->firstchild;
-          while (pred->succ != NULL) {
-            pred = pred->succ;
-          }
-          // create new directory
-          struct node *succ = malloc(sizeof(struct node));
-          strcpy(succ->name, buffer + 4);
-          succ->value = 0;
-          succ->parent = subtree;
-          succ->pred = pred;
-          succ->succ = succ->firstchild = NULL;
-          // append new directory to pred
-          pred->succ = succ;
-          // build subtree of succ
-          // recursive call here
-          // clean up
-          // fsetpos(fptr, directory_pos);
-          // free(directory_pos);
-        }
-        // go to next line
-        fgets(buffer, BUFFERSZ, fptr);
-        printf("%s", buffer);
-      }
-    }
-  }
+void print_directory_tree(struct node *root, int indent) {
+  struct node *curr = root;
 
-  return 0;
+  for (int i = 0; i < indent; i++) {
+    printf("-");
+  }
+  printf(" %d %s", curr->size, curr->name);
+  for (int i = 0; i < curr->num_children; i++) {
+    print_directory_tree(&curr->children[i], indent + 1);
+  }
 }
 
 int main(int argc, char const *argv[]) {
+  int sum = 0;
+  fpos_t *fpos = malloc(sizeof(fpos_t));
   FILE *fptr = fopen(FILEPATH, "r");
   struct node *root = malloc(sizeof(struct node));
+  char buffer[BUFFERSZ] = {'\0'};
   if (fptr == NULL) {
     return -1;
   }
 
-  strcpy(root->name, (char *)"/\0");
-  root->value = 0;
-  root->pred = root->succ = root->parent = root->firstchild = NULL;
+  // prepare root node
+  fgets(buffer, BUFFERSZ, fptr); // read first line $ cd /
+  strcpy(root->name, buffer + 5);
+  root->size = 0;
+  root->num_children = 0;
+  root->children = malloc(sizeof(struct node*));
 
-  fpos_t *start = malloc(sizeof(fpos_t));
-  fgetpos(fptr, start);
+  // remember start position
+  fgetpos(fptr, fpos);
 
-  build_directory_tree(fptr, root);
-  print_directory_tree(root);
-  // free_directory_tree(root);
-  // clean_up_tree(root);
+  generate_directory(fptr, fpos, buffer, buffer, root);
+  print_directory_tree(root, 0);
+  printf("\n");
+  calculate_total_size_of_directories(root, &sum);
+
+  printf("Sum: %d", sum);
+
   free(root);
-
-  fsetpos(fptr, start);
-  
-  free(start);
+  free(fpos);
   fclose(fptr);
   return 0;
 }
